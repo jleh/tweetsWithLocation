494afb44c5151b5a1fcd50e5336bdeac432d98a1
jleh <jupelehtinen@gmail.com>
2016-01-24 18:20:50 +0200

    cross-origin

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ var client = new Twitter({
   access_token_secret: ''
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
     getTweet(function (data) { res.send(data); });
 });
